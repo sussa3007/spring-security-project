2654fd812cce444326029ccd98ba4a3331eec03a
sussa3007 <sussa1933@gmail.com>
2022-11-21 01:03:34 +0900

    Add #1 - Filter
    # tester 유저에게 어드민과 유저권한을 모두 부여하도록 하는 필터 추가

--- src/main/java/com/suyoung/springsecurityproject/config/SpringSecurityConfig.java
+++ src/main/java/com/suyoung/springsecurityproject/config/SpringSecurityConfig.java
@@ -1,27 +1,39 @@
 package com.suyoung.springsecurityproject.config;
 
-import com.suyoung.springsecurityproject.user.UserService;
+import com.suyoung.springsecurityproject.filter.TesterAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
-@RequiredArgsConstructor
-public class SpringSecurityConfig  {
+public class SpringSecurityConfig {
 
-    private final UserService userService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // basic authentication
+//        http.httpBasic(); // basic authentication filter 활성화
+        http.httpBasic().disable(); // basic authentication filter 비활성화
+
+        // csrf
+        http.csrf();
+
+        // remember-me
+        http.rememberMe();
+
+        //tester authentication filter
+        http.apply(new CustomFilterConfig());
         // permit
         http.authorizeHttpRequests()
-                .antMatchers("/", "/css/**", "/home", "/signup").permitAll()
-                // hello 페이지는 USER 롤을 가진 유저에게만 허용
+                .antMatchers("/","/images/**", "/css/**", "/home", "/signup","/login").permitAll()
                 .antMatchers("/post").hasRole("USER")
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/notice").hasRole("ADMIN")
@@ -36,16 +48,20 @@ public class SpringSecurityConfig  {
         http.logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/");
-        // basic authentication
-//        http.httpBasic(); // basic authentication filter 활성화
-        http.httpBasic().disable(); // basic authentication filter 비활성화
 
-        // csrf
-        http.csrf();
-
-        // remember-me
-        http.rememberMe();
         return http.build();
+    }
+
+
+    public class CustomFilterConfig extends AbstractHttpConfigurer<CustomFilterConfig, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            System.out.println("# Do filter");
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+            TesterAuthenticationFilter testerAuthenticationFilter = new TesterAuthenticationFilter(authenticationManager);
+            testerAuthenticationFilter.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/login","POST"));
+            builder.addFilter(testerAuthenticationFilter);
+        }
     }
 
 
