62abe0dbd658a454fee7af3f2dded7e0993077db
sussa3007 <sussa1933@gmail.com>
2022-11-20 18:49:40 +0900

    Add #1 - Exception
    # 예외 추가

---
+++ src/main/java/com/suyoung/springsecurityproject/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.suyoung.springsecurityproject.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException() {
+        super("유저를 찾을 수 없습니다.");
+    }
+}
