9eabe1f8243e1543bd398f9bcc37200e1719d915
sussa3007 <sussa1933@gmail.com>
2022-11-20 15:45:19 +0900

    Add #1 - Admin, User
    # 어드민과 유저  View에 따른 설정 추가
    # 어플리케이션 실행시 기본 유저,어드민 추가

--- src/main/java/com/suyoung/springsecurityproject/config/SpringSecurityConfig.java
+++ src/main/java/com/suyoung/springsecurityproject/config/SpringSecurityConfig.java
@@ -1,52 +1,65 @@
 package com.suyoung.springsecurityproject.config;
 
+import com.suyoung.springsecurityproject.user.User;
+import com.suyoung.springsecurityproject.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.annotation.PostConstruct;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SpringSecurityConfig  {
+
+    private final UserService userService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // permit
         http.authorizeHttpRequests()
-                .antMatchers("/", "/home","/example").permitAll()
+                .antMatchers("/", "/css/**", "/home", "/example", "/signup").permitAll()
                 // hello 페이지는 USER 롤을 가진 유저에게만 허용
-                .antMatchers("/private").hasRole("USER")
+                .antMatchers("/post").hasRole("USER")
                 .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated();
         // login
         http.formLogin() // 기본 로그인 인증 제공
+                .loginPage("/login")
                 .defaultSuccessUrl("/")
-                .permitAll() // 모두 허용
-                .and()
-                .logout();
+                .permitAll(); // 모두 허용
+        // logout
+        http.logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/");
         return http.build();
     }
     @Bean
     public UserDetailsService userDetailsService() {
-        PasswordEncoder encoder = passwordEncoder();
-        // user 정보
-        UserDetails user = User.withUsername("user")
-                .password(encoder.encode("user"))
-                .roles("USER") // user에 ROLE_USER라는 롤이 추가됩니다. "ROLE_" 은 자동으로 추가됩니다.
-                .build();
-        // admin 정보
-        UserDetails admin = User.withUsername("admin")
-                .password(encoder.encode("admin"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+        return username -> {
+            User user = userService.findByUsername(username);
+            if (user == null) {
+                throw new UsernameNotFoundException(username);
+            }
+            return user;
+        };
+    }
+    @Bean
+    @PostConstruct
+    public void adminAccount() {
+        userService.signup("user", "user");
+        userService.signupAdmin("admin", "admin");
     }
 
     @Bean
